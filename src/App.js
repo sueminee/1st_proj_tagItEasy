8fbeada71d5a55d693a77aadcc05a5a0e13c2f9a
SUEMIN JEONG <sueminee@SUEMINui-MacBook-Air.local>
2018-08-04 15:54:49 +0900

    진짜

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-<<<<<<< HEAD
 import Login from './sign-form/01_logIn';
 import Signup from './sign-form/02_signUp';
-=======
-import './css/App.css';
-import './img/logo.png'
-import Auth from './sign-form/01_auth';
->>>>>>> 8089a57799637adf48fa2912c4f5101c05d9f945
 import Home from './main/01_home';
 import './css/App.css';
 
